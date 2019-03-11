e1a617a50f92deb207bbd80af09a056479cc1fa2
Hakim Kawesi <hakimks@gmail.com>
2019-03-11 17:01:22 +0300

    feat: Add user model, usercreate form view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverrse_lazy
+from django.views.generic import CreateView
+from . import forms
 
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverrse_lazy('login')
+    template_name = 'accounts/signup.html'
