8927e2bc6fb7f8c6dafa52b510f3bd462c5a6dc3
Hakim Kawesi <hakimks@gmail.com>
2019-03-25 13:56:41 +0300

    feat: added Leave and join group views plus adding them to the urls.py file

--- groups/urls.py
+++ groups/urls.py
@@ -6,5 +6,7 @@ app_name = 'groups'
 urlpatterns = [
     url(r'^$', views.ListGroups.as_view(), name='all'),
     url(r'^new/$', views.CreateGroup.as_view(), name='create'),
-    url(r'^posts/in/(?p<slug>[-\w]+)/$', views.SingleGroup.as_view(), name='single')
+    url(r'^posts/in/(?p<slug>[-\w]+)/$', views.SingleGroup.as_view(), name='single'),
+    url(r'join/(?p<slug>[-\w]+)/$', views.JoinGroup.as_view(), name='join'),
+    url(r'leave/(?p<slug>[-\w]+)/$', views.LeaveGroup.as_view(), name='leave'),
 ]
